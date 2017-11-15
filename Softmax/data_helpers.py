d399081ac7e6e5efae2de5a9844b2b5ce77884a6
GongYanshang <gongyanshang@163.com>
2017-11-16 00:33:19 +0800

    完成读取CIFAR10数据模块

--- Softmax/data_helpers.py
+++ Softmax/data_helpers.py
@@ -1,10 +1,84 @@
-# ¶ÁÈ¡CIFAR10Êı¾İ
+# è¯»å–CIFAR10æ•°æ®
 import numpy as np
-import pickle # save/reload¹¦ÄÜ
-import sys # ¸ºÔğ³ÌĞòÓë½âÊÍÆ÷µÄ½»»¥£¬Èç±ê×¼ÊäÈëÊä³öµÈ
+import pickle # save/reloadåŠŸèƒ½
+import sys # è´Ÿè´£ç¨‹åºä¸è§£é‡Šå™¨çš„äº¤äº’ï¼Œå¦‚æ ‡å‡†è¾“å…¥è¾“å‡ºç­‰
 
-# ´ÓÒ»¸öCIFAR10ÎÄ¼şÖĞ¶ÁÈ¡Êı¾İ
+# ä»ä¸€ä¸ªCIFAR10æ–‡ä»¶ä¸­è¯»å–æ•°æ®
 def load_CIFAR10_batch(filename):
-    with open(filename, 'rb')
+    with open(filename, 'rb') as f:
+        if sys.version_info[0] < 3: # sys.version_info(major=3, minor=5, micro=2, releaselevel='final', serial=0) ç‰ˆæœ¬ï¼š3.5.2
+            dict = pickle.load(f)
+        else:
+            dict = pickle.load(f, encoding='latin1')
+        x = dict['data']
+        y = dict['labels']
+        x = x.astype(float) # ç±»å‹è½¬æ¢ï¼Œè½¬æ¢ä¸ºfloatç±»å‹
+        y = np.array(y)
+    return x, y
 
+# è¯»å–æ‰€æœ‰CIFAR10çš„æ•°æ®
+def load_data():
+    
+    # è¯»å–è®­ç»ƒé›†
+    xs = []
+    ys = []
+    for i in range(1,6):
+        filename = 'cifar-10-batches-py/data_batch_' + str(i)
+        X, Y = load_CIFAR10_batch(filename)
+        xs.append(X)
+        ys.append(Y)
+    x_train = np.concatenate(xs)
+    y_train = np.concatenate(ys)
+    del xs, ys
+    
+    # è¯»å–æµ‹è¯•é›†
+    x_test, y_test = load_CIFAR10_batch('cifar-10-batches-py/test_batch')
+    
+    # åä¸ªç±»åˆ«
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    
+    # æ•°æ®å½’ä¸€åŒ–
+    mean_image = np.mean(x_train, axis=0)
+    x_train -= mean_image
+    x_test -= mean_image
+    
+    data_dict = {
+        'images_train': x_train,
+        'labels_train': y_train,
+        'images_test': x_test,
+        'labels_test': y_test,
+        'classes': classes
+    }
+    return data_dict
 
+def reshape_data(data_dict):
+    im_tr = np.array(data_dict['images_train'])
+    im_tr = np.reshape(im_tr, (-1, 3, 32, 32))
+    im_tr = np.transpose(im_tr, (0, 2, 3, 1))
+    data_dict['images_train'] = im_tr
+    im_te = np.array(data_dict['images_test'])
+    im_te = np.reshape(im_te, (-1, 3, 32, 32))
+    im_te = np.transpose(im_te, (0,2,3,1))
+    data_dict['images_test'] = im_te
+    return data_dict
+
+def gen_batch(data, batch_size, num_iter):
+    data = np.array(data)
+    index = len(data)
+    for i in range(num_iter):
+        index += batch_size
+        if(index + batch_size > len(data)):
+            index = 0
+            shuffled_indices = np.random.permutation(np.arange(len(data)))
+            data = data[shuffled_indices]
+        yield data[index:index + batch_size]
+        
+def main():
+    data_sets = load_data()
+    print(data_sets['images_train'].shape)
+    print(data_sets['labels_train'].shape)
+    print(data_sets['images_test'].shape)
+    print(data_sets['labels_test'].shape)
+
+if __name__ == '__main__':
+    main()
