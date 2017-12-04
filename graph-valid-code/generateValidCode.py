ff63ba111f871ebef40fb588eb791b71a0bf575c
GongYanshang <gongyanshang@163.com>
2017-12-05 00:20:04 +0800

    随机生成验证码增加圆点输出

--- graph-valid-code/generateValidCode.py
+++ graph-valid-code/generateValidCode.py
@@ -2,32 +2,37 @@
 import random
 from PIL import Image,ImageDraw,ImageFont
 
-# 创建图片，400x100 px，RGB=(255,255,255)
-img1 = Image.new(mode="RGB",size=(400,100),color=(255,255,255))
+# 创建图片
+width = 400
+height = 100
+img1 = Image.new(mode="RGB",size=(width,height),color=(255,255,255))
 
 # 创建画笔
 draw1 = ImageDraw.Draw(img1, mode="RGB")
 
 # 定义要使用的字体
 font1 = ImageFont.truetype('simsun.ttc',98)
-
+code = [0]*5 # 长度为5的数组，初始值为0
 for i in range(5):
     # 随机从a~z或者0~9中抽取一个字母或数字。char()表示将ASCII码转为字符
     char1 = random.choice([chr(random.randint(65,90)),str(random.randint(0,9))])
+    code[i] = char1
 
     # 随机生成颜色
     color1 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
     
     # 把生成的字母或数字添加到图片上
-    draw1.text([i*80,0],char1,color1,font=font1)
+    draw1.text([i*(400/5),0],char1,color1,font=font1)
     
-    # 生成一个红点一个黑点
-    draw1.point([100,100],fill="red")
-    draw1.point([80,80],fill=(0,0,0))
-    
-    # 画两条线，前两个数是开始坐标，后两个数是结束坐标
-    draw1.line((100,50,300,50),fill="red")
-    draw1.line((100,60,200,10),fill="blue")
+    # 生随机生成10个点,随机生成10条线
+    radio = random.randint(3,8) # 圆点的半径
+    for j in range(10):
+        c1,c2 = (random.randint(radio,width-radio),random.randint(radio,height-radio)) # 圆心位置
+        draw1.ellipse((c1-radio,c2-radio,c1+radio,c2+radio), fill = color1, outline =color1)
+        draw1.line((random.randint(0,400),random.randint(0,100),random.randint(0,400),random.randint(0,100)),fill=color1)
+
+# 验证码输出
+print("验证码为",code)
 
 # 保存
 with open("pic.png","wb") as f:
