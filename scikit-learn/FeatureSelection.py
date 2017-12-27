e21b27cf4ab45a67b0d4498d4bbf5f0189fdcea7
GongYanshang <gongyanshang@163.com>
2017-12-27 23:27:15 +0800

    sklearn增加特征提取

--- scikit-learn/FeatureSelection.py
+++ scikit-learn/FeatureSelection.py
@@ -1,9 +1,22 @@
 from sklearn import metrics
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
 import DataLoading
+import numpy as np
 
 X, y = DataLoading.load_data()
+print(np.shape(X))
+
+# Tree algorithms allow to compute the informativeness of features.
 model = ExtraTreesClassifier()
 model.fit(X,y)
+print(model.feature_importances_)
 
-print(model.feature_importances)
+# create the RFE model and select 3 attributes
+model = LogisticRegression()
+rfe = RFE(model, 3)
+rfe = rfe.fit(X, y)
+# summarize the selection of the attributes
+print(rfe.support_)
+print(rfe.ranking_)
