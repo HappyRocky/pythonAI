394aba6179970f1af67ab5e8393dbe96c2af009f
GongYanshang <gongyanshang@163.com>
2017-12-28 23:59:47 +0800

    算法训练：查询子字符串第一次出现的位置，最传统方法

---
+++ algorithm-exercise/FindSubStr.py
@@ -0,0 +1,22 @@
+# 问题：给定两个字符串A和B，要求判断A是否包含B，如果包含返回第一次出现B的index
+class FindSubStr:
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+        
+    # 最传统的方法，双重循环，O(m*n)
+    def strStr(self):
+        if self.source is None or self.target is None:
+            return -1
+        for i in range(len(self.source) - len(self.target) + 1):
+            for j in range(len(self.target)):
+                if self.source[i + j] != self.target[j]:
+                    match = False
+                    break
+            else:
+                return i
+        return -1
+
+if __name__ == '__main__':
+    S = FindSubStr('gongyanshang','yan')
+    print(S.strStr())
