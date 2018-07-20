3a304cddfc29203953981a179bec0b4f15647ba1
gongyanshang1 <gongyanshang1@360buyad.local>
2018-07-20 15:14:46 +0800

    测试flask

---
+++ flask/helloword.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 20 14:28:02 2018
+
+@author: gongyanshang1
+
+"""
+
+from flask import Flask
+from flask import request
+
+# Flask初始化参数尽量使用你的包名，这个初始化方式是官方推荐的，官方解释：http://flask.pocoo.org/docs/0.12/api/#flask.Flask
+app = Flask(__name__)
+
+
+@app.route('/hw/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+if __name__ == "__main__":
+    # 这种是不太推荐的启动方式，我这只是做演示用，官方启动方式参见：http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
+    app.run(debug=True)
+
