36193d77e45f93ad1225d755d259b96a23e5f801
gongyanshang1 <gongyanshang1@360buyad.local>
2018-08-22 14:56:04 +0800

    最长回文子串增加求公共子串的方法

--- LeetCode/5_Longest_Palindromic_Substring.py
+++ LeetCode/5_Longest_Palindromic_Substring.py
@@ -89,11 +89,48 @@ def longestPalindrome2(s):
                 max_substr = substr
     return max_substr
 
+def longestPalindrome3(s):
+    """
+    :type s: str
+    :rtype: str
+    
+    求原字符串和逆字符串的公共子串。
+    1、求公共子串可以使用动态规划方法，详情见 https://github.com/HappyRocky/pythonAI/blob/master/algorithm-exercise/find_max_same_substring.py
+    2、不是所有的公共子串都符合要求，还需要判断原字符串翻转之后是否还是同一个子串。
+    时间复杂度为O(n^2)。
+    """
+    if len(s) <= 1:
+        return s
+    
+    # 翻转
+    s1 = s
+    s2 = s[::-1]
+    
+    # 矩阵，高为s1长度，宽为s2长度
+    flag_matrix = []
+    max_len = 0
+    last_idx = 0
+    for i in range(len(s1)):
+        flag_matrix.append([])
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                if i > 0 and j > 0:
+                    flag_matrix[i].append(flag_matrix[i-1][j-1] + 1)
+                else:
+                    flag_matrix[i].append(1)
+            else:
+                flag_matrix[i].append(0)
+            # 比已知的最大长度还长，且：s1中子串起始位置距离原字符串的起始位置 == s2中子串的末尾位置距离原字符串的末尾位置  
+            if flag_matrix[i][j] > max_len and i - flag_matrix[i][j] + 1 == len(s2) - j - 1:
+                max_len = max(max_len, flag_matrix[i][j])
+                last_idx = i
+    return s1[last_idx-max_len+1 : last_idx+1]
+
 
 if '__main__' == __name__:
-    s_list = ["cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj"]
+    s_list = ["cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"]
     for s in s_list:
-        result = longestPalindrome2(s)
+        result = longestPalindrome3(s)
         print(f'{s}\t{result}')
                 
     
