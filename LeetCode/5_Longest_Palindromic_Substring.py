5aee2f13a691957784f6dd3255bd7789332a503f
gongyanshang1 <gongyanshang1@360buyad.local>
2018-08-24 14:37:03 +0800

    实现马拉车算法

--- LeetCode/5_Longest_Palindromic_Substring.py
+++ LeetCode/5_Longest_Palindromic_Substring.py
@@ -175,10 +175,55 @@ def longestPalindrome4(s):
         
     return max_str
 
+def longestPalindrome5(s):
+    """
+    :type s: str
+    :rtype: str
+    
+    马拉车算法。Manacher发明出来的。
+    时间复杂度为O(n)。
+    """
+    if len(s) <= 1:
+        return s
+    
+    # 每个字符之间插入 \1
+    ss = '\0\1' + '\1'.join([x for x in s]) + '\1\2'
+    p = [0] * len(ss)
+    center = 0
+    mx = 0
+    max_str = ''
+    for i in range(1, len(p)-1):
+        
+        # 初始化 p[i]
+        if i > mx:
+            p[i] = 0
+        else:
+            j = 2 * center - i # i 关于 center 的对称点
+            p[i] = min(mx-i, p[j])
+        
+        # 尝试继续向两边扩展，更新 p[i]
+        try:
+            while ss[i - p[i] - 1] == ss[i + p[i] + 1]: # 不必判断是否溢出，因为首位均有特殊字符，肯定会退出
+                p[i] += 1
+        except:
+            print(i + p[i] + 1)
+            
+        # 更新中心
+        if i + p[i] > mx:
+            mx = i + p[i]
+            center = i
+            
+        # 更新最长串
+        if 1 + 2 * p[i] > len(max_str):
+            max_str = ss[i - p[i] : i + p[i] + 1]
+            
+    return max_str.replace('\1', '')
+        
+
 if '__main__' == __name__:
     s_list = ["babad","cbbd"]
     for s in s_list:
-        result = longestPalindrome4(s)
+        result = longestPalindrome5(s)
         print(f'{s}\t{result}')
                 
     
