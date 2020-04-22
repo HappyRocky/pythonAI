bd1bd0cdb9538da0084e60907ceb50e9c4f53e9a
Rocky <gongyanshang@163.com>
2020-04-22 22:30:25 +0800

    leetcode166

--- LeetCode/166_Fraction_to_Recurring_Decimal.py
+++ LeetCode/166_Fraction_to_Recurring_Decimal.py
@@ -23,6 +23,7 @@ Input: numerator = 2, denominator = 3
 Output: "0.(6)"
 """
 
+
 def fractionToDecimal(numerator: int, denominator: int) -> str:
     """
     实现除法。
@@ -32,5 +33,36 @@ def fractionToDecimal(numerator: int, denominator: int) -> str:
     # 记录正负号
     flag = '-' if numerator * denominator < 0 else ''
     numerator = abs(numerator)
-    denominator = abs(numerator)
+    denominator = abs(denominator)
 
+    # 是否可以整除
+    result_list = [str(numerator // denominator)]
+    reminder = numerator % denominator
+    if reminder == 0:  # 可以整除
+        return flag + ''.join(result_list)
+
+    # 不可以整除，开始循环
+    result_list.append('.')
+    result_map = {}  # 存放每一位的位置，便于发现循环起始点
+
+    while reminder != 0:
+
+        # 结果之前曾经出现过，说明进入了循环
+        if reminder in result_map:
+            result_list.insert(result_map[reminder], '(')
+            result_list.append(')')
+            return flag + ''.join(result_list)
+
+        result_map[reminder] = len(result_list)  # 记录所在位置
+        reminder *= 10  # 借位
+        result = str(reminder // denominator)
+        reminder = reminder % denominator
+        result_list.append(result)
+
+    return flag + ''.join(result_list)
+
+
+if '__main__' == __name__:
+    numerator = 1
+    denominator = 33
+    print(fractionToDecimal(numerator, denominator))
